802c1b0e658f9c365cc460ae7633174879768c45
winthihasoe <winthihasoe@gmial.com>
2021-03-01 14:19:46 +0630

    adding css and icons to project

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="project">
+        <div class="flexing">
+            <div @click="showDetail=!showDetail">
+                <h3>{{project.title}}</h3>
+            </div>
+            <div>
+                <span class="material-icons">
+                delete
+                </span>
+                <span class="material-icons">
+                edit
+                </span>
+                <span class="material-icons">
+                done
+                </span>
+            </div>
+        </div>
+        
+        <p v-if='showDetail'>{{project.detail}}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props:["project"],
+    data(){
+        return {
+            showDetail:false
+        }
+    }
+}
+</script>
+
+<style>
+    .project{
+        background-color: #f2f2f2;
+        padding: 15px;
+        margin: 10px;
+        text-align: left;
+    }
+
+    h3{
+        color: indigo;
+        cursor: pointer;
+
+    }
+
+    .flexing{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    span{
+        margin-left: 10px;
+    }
+    span:hover{
+        color: #777;
+        cursor: pointer;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <single-project :project="project"></single-project>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject.vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SingleProject
+  },
+  data(){
+    return {
+      projects:[] //[{…}, {…}, {…}]
+    }
+  },
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then((response)=>{
+      return response.json()
+  })
+    .then((datas)=>{
+      this.projects=datas
+  })
+    .catch(()=>{
+
+    })
   }
 }
 </script>
